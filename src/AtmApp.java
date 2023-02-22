1274bb490451d4d8f4cb01a2945e077801d28ae2
Nagihan <nagihanglk@gmail.com>
2023-02-22 22:32:39 +0300

    nghn1

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -5,6 +5,7 @@ public class AtmApp {
         System.out.println("Java is easy");
         System.out.println("Learn Java Earn Money");
         System.out.println("giti ogrenmek cok kolay");
+        System.out.println("nagihan branchi yeni bir ozellik ekledi.");
 
     }
 }
